2d1fdd8abe1bede259dded02dac603089a279880
TrongLuan <=>
2024-06-15 17:02:07 +0700

    example ads

--- app/src/main/java/com/mia/module/MainActivity.java
+++ app/src/main/java/com/mia/module/MainActivity.java
@@ -3,36 +3,70 @@ package com.mia.module;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.ads.mia.ads.MiaAd;
 import com.ads.mia.ads.wrapper.ApInterstitialAd;
 import com.ads.mia.funtion.AdCallback;
+import com.facebook.shimmer.ShimmerFrameLayout;
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.LoadAdError;
 
 public class MainActivity extends AppCompatActivity {
     private ApInterstitialAd mInterstitialAd;
-    private Button btnShow;
+    private Button btnLoad, btnShow;
+    private FrameLayout frAds;
+    private ShimmerFrameLayout shimmerAds;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MiaAd.getInstance().getInterstitialAds(this, BuildConfig.ad_interstitial_splash, new AdCallback(){
+
+        btnLoad = findViewById(R.id.btnLoad);
+        btnShow = findViewById(R.id.btnShow);
+        frAds = findViewById(R.id.fr_ads);
+        shimmerAds = findViewById(R.id.shimmer_native);
+
+
+        // Interstitial Ads
+        btnLoad.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onApInterstitialLoad(@Nullable ApInterstitialAd apInterstitialAd) {
-                super.onApInterstitialLoad(apInterstitialAd);
-                mInterstitialAd = apInterstitialAd;
+            public void onClick(View view) {
+                MiaAd.getInstance().getInterstitialAds(MainActivity.this, BuildConfig.ad_interstitial_splash, new AdCallback() {
+                    @Override
+                    public void onApInterstitialLoad(@Nullable ApInterstitialAd apInterstitialAd) {
+                        super.onApInterstitialLoad(apInterstitialAd);
+                        mInterstitialAd = apInterstitialAd;
+                        Toast.makeText(MainActivity.this, "Ads Ready", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
+        btnShow.setOnClickListener(v -> MiaAd.getInstance().forceShowInterstitial(MainActivity.this, mInterstitialAd, new AdCallback() {
+        }, true));
 
-        btnShow = findViewById(R.id.btnShow);
-        btnShow.setOnClickListener(new View.OnClickListener() {
+        // Banner Ads
+        MiaAd.getInstance().loadBanner(this, BuildConfig.ad_banner);
+        /*MiaAd.getInstance().loadCollapsibleBanner(this, BuildConfig.ad_banner, AppConstant.CollapsibleGravity.BOTTOM, new AdCallback());*/
+
+        // Native Ads
+        MiaAd.getInstance().loadNativeAd(this, BuildConfig.ad_native, R.layout.native_large, frAds, shimmerAds, new AdCallback() {
             @Override
-            public void onClick(View v) {
-                MiaAd.getInstance().forceShowInterstitial(MainActivity.this, mInterstitialAd, new AdCallback(){
+            public void onAdFailedToLoad(@Nullable LoadAdError i) {
+                super.onAdFailedToLoad(i);
+                frAds.removeAllViews();
+            }
 
-                }, false);
+            @Override
+            public void onAdFailedToShow(@Nullable AdError adError) {
+                super.onAdFailedToShow(adError);
+                frAds.removeAllViews();
             }
         });
 
