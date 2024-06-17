00a77e2b157fe71631c1a5790fcaad7210ba6f28
Luan Dinh <trongluan8686@gmail.com>
2024-06-17 09:34:05 +0700

    example reward

--- app/src/main/java/com/mia/module/MainActivity.java
+++ app/src/main/java/com/mia/module/MainActivity.java
@@ -16,16 +16,21 @@ import com.ads.mia.ads.wrapper.ApNativeAd;
 import com.ads.mia.billing.AppPurchase;
 import com.ads.mia.funtion.AdCallback;
 import com.ads.mia.funtion.PurchaseListener;
+import com.ads.mia.funtion.RewardCallback;
 import com.facebook.shimmer.ShimmerFrameLayout;
 import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
 
 public class MainActivity extends AppCompatActivity {
     private ApInterstitialAd mInterstitialAd;
-    private Button btnLoad, btnShow, btnIap;
+    private Button btnLoad, btnShow, btnIap, btnLoadReward, btnShowReward;
     private FrameLayout frAds;
     private ShimmerFrameLayout shimmerAds;
     private ApNativeAd mApNativeAd;
+    private RewardedAd rewardedAds;
+    private boolean isEarn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
         btnLoad = findViewById(R.id.btnLoad);
         btnShow = findViewById(R.id.btnShow);
         btnIap = findViewById(R.id.btnIap);
+        btnLoadReward = findViewById(R.id.btnLoadReward);
+        btnShowReward = findViewById(R.id.btnShowReward);
         frAds = findViewById(R.id.fr_ads);
         shimmerAds = findViewById(R.id.shimmer_native);
 
@@ -124,6 +131,46 @@ public class MainActivity extends AppCompatActivity {
         });
 
         btnIap.setOnClickListener(v -> AppPurchase.getInstance().purchase(MainActivity.this, "android.test.purchased"));
+
+        // Reward Ads
+        btnLoadReward.setOnClickListener(v -> {
+            MiaAd.getInstance().initRewardAds(this, BuildConfig.ad_reward, new AdCallback() {
+                @Override
+                public void onRewardAdLoaded(RewardedAd rewardedAd) {
+                    super.onRewardAdLoaded(rewardedAd);
+                    rewardedAds = rewardedAd;
+                    Toast.makeText(MainActivity.this, "Ads Ready", Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
+
+        btnShowReward.setOnClickListener(v -> {
+            isEarn = false;
+            MiaAd.getInstance().showRewardAds(MainActivity.this, rewardedAds, new RewardCallback() {
+                @Override
+                public void onUserEarnedReward(RewardItem var1) {
+                    isEarn = true;
+                }
+
+                @Override
+                public void onRewardedAdClosed() {
+                    if (isEarn) {
+                        // action intent
+                    }
+                }
+
+                @Override
+                public void onRewardedAdFailedToShow(int codeError) {
+
+                }
+
+                @Override
+                public void onAdClicked() {
+
+                }
+            });
+        });
+
 
     }
 }
